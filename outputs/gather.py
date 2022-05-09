4d10b3b64e2285c99beae609f0788afee03c3774
chenxin <chenxin18@mails.tsinghua.edu.cn>
2022-05-10 02:36:54 +0800

    add gather.py

---
+++ outputs/gather.py
@@ -0,0 +1,30 @@
+import csv
+
+
+def gather():
+    input_path = [
+        'results-LuisaRender.csv',
+        'results-Mitsuba2.csv',
+        'results-PBRT-v4.csv',
+    ]
+    output_path = 'results-all.csv'
+    headers = []
+    data = []
+    for path in input_path:
+        with open(path, 'r') as f:
+            f_csv = csv.reader(f)
+            line = 0
+            for row in f_csv:
+                if line == 0:
+                    headers = row
+                else:
+                    data.append(row)
+                line += 1
+    with open(output_path, 'w') as f:
+        f_csv = csv.writer(f)
+        f_csv.writerow(headers)
+        f_csv.writerows(data)
+
+
+if __name__ == '__main__':
+    gather()
